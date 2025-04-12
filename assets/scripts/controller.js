b64a9d298f9a19c70089af919b8fd703705f00f5
MariaValeria-Sandru <valy895@yahoo.com>
2025-04-12 11:36:00 +0200

    ajouter controller

---
+++ assets/scripts/controller.js
@@ -0,0 +1,9 @@
+import {recipes} from "./data/recipes.js";
+import {View} from "./view.js"
+
+export default class Controller {
+    constructor() {
+        let view = new View
+        // view.displayRecipes(recipes)
+    }
+}
